787c79fc9d46b31f6849f1e44f1eb9cdb83d1e7f
Gary Blok <gary@thebloks.com>
2025-09-25 10:39:15 -0500

    Add Pre-Req FUnctions

--- DeployR/DeployRFunctions.ps1
+++ DeployR/DeployRFunctions.ps1
@@ -14,6 +14,37 @@ function Get-CMOSDGather {
     $Script | Out-File -FilePath "$env:temp\CMOSD-Gather.ps1" -Force -Encoding UTF8
     powershell.exe "$env:temp\CMOSD-Gather.ps1" -debug $true
 }
+write-host "===================================================================="
+write-Host "Install Pre-Reqs Functions" -ForegroundColor Green
+Write-Host -ForegroundColor Green "[+] Install-WindowsADK"
+function Install-WindowsADK {
+    iex (irm https://raw.githubusercontent.com/materrill/miketerrill.net/refs/heads/master/Software%20Install%20Scripts/Install-WindowsADK.ps1)
+}
+Write-Host -ForegroundColor Green "[+] Install-WinPEAddOn"
+function Install-WinPEAddOn {
+    iex (irm https://raw.githubusercontent.com/materrill/miketerrill.net/refs/heads/master/Software%20Install%20Scripts/Install-WinPEAddOn.ps1)
+}
+Write-Host -ForegroundColor Green "[+] Install-VCRedist-x64"
+function Install-VCRedist-x64 {
+    iex (irm https://raw.githubusercontent.com/materrill/miketerrill.net/refs/heads/master/Software%20Install%20Scripts/Install-VCRedist-x64.ps1)
+}
+write-Host -ForegroundColor Green "[+] Install-SQLExpress2022"
+function Install-SQLExpress2022 {
+    iex (irm https://raw.githubusercontent.com/materrill/miketerrill.net/refs/heads/master/Software%20Install%20Scripts/Install-SQLExpress2022.ps1)
+}
+write-Host -ForegroundColor Green "[+] Configure-SQLExpress"
+function Configure-SQLExpress { 
+    iex (irm https://raw.githubusercontent.com/materrill/miketerrill.net/refs/heads/master/Software%20Install%20Scripts/Configure-SQLExpress.ps1)
+}
+write-Host -ForegroundColor Green "[+] Install-SSMS21"
+function Install-SSMS21 {
+    iex (irm https://raw.githubusercontent.com/materrill/miketerrill.net/refs/heads/master/Software%20Install%20Scripts/Install-SSMS21.ps1)
+}
+
+
+
+
+write-Host "===================================================================="
 write-host "Function: Invoke-DeployRTS" -ForegroundColor Green
 write-host " Common Servers I Use:" -ForegroundColor magenta
 write-host "  - 214-deployr.2p.garytown.com" -ForegroundColor Green
