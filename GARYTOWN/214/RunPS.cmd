96bb449ea0c74541d96fa2163d818436379c87c0
Gary Blok <gary@thebloks.com>
2025-10-01 12:19:53 -0500

    fix spelling

--- GARYTOWN/214/RunPS.cmd
+++ GARYTOWN/214/RunPS.cmd
@@ -3,7 +3,7 @@ REM - this CMD file checks the platform (x86/64) and then runs the correct PS co
 
 PUSHD %~dp0
 If "%PROCESSOR_ARCHITEW6432%"=="AMD64" GOTO 64bit
-PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command ".\StifleR_Client_Wrapper214.ps1"
+PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command ".\StifleR_Client_Wrapper214-ConfigMgr.ps1"
 GOTO END
 :64bit
 "%WinDir%\Sysnative\windowsPowershell\v1.0\Powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command ".\StifleR_Client_Wrapper214-ConfigMgr.ps1"
