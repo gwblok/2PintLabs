9ad055489a51d48570f7cf072fbc571d64dc1b4b
gwblok <gary@thebloks.com>
2026-03-20 10:23:16 -0500

    Cleanup

--- DeployR/ServerSideScripts/Create-DeployRApp.ps1
+++ DeployR/ServerSideScripts/Create-DeployRApp.ps1
@@ -9,3 +9,21 @@ Function New-DeployRApp {
     $NewDRCI = New-DeployRContentItem -Type Folder -Name $AppName -Description $AppDescription -Purpose Application
     New-DeployRContentItemVersion -ContentItemId $NewDRCI.id -SourceFolder $AppSourceFolder -InstallationCommandLine $InstallationCommandLine
 }
+
+Function Update-DeployRApp {
+    Param (
+        [string]$AppName,
+        [string]$AppSourceFolder,
+        [string]$AppDescription = "No Description Provided",
+        [string]$InstallationCommandLine = ""
+    )
+
+    $DRCI = Get-DeployRContentItem -Purpose Application | Where-Object { $_.name -eq $AppName }
+    if ($DRCI) {
+        Write-Host "Updating application '$AppName' (ID: $($DRCI.id)) with new version from source folder '$AppSourceFolder'..." -ForegroundColor Green
+        #New-DeployRContentItemVersion -ContentItemId $DRCI.id -SourceFolder $AppSourceFolder -InstallationCommandLine $InstallationCommandLine
+    }
+    else {
+        Write-Error "Application '$AppName' not found. Cannot update non-existent application."
+    }
+}
