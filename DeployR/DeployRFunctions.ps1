25c3b09cee331a6a40617e07b1a275e39a27d017
gwblok <gary@thebloks.com>
2026-07-27 09:37:53 -0500

    convert to function

--- DeployR/DeployRFunctions.ps1
+++ DeployR/DeployRFunctions.ps1
@@ -71,12 +71,15 @@ Write-Host -ForegroundColor Green "[+] Enable-RequiredWindowsOptionalFeatures"
 function Enable-RequiredWindowsOptionalFeatures {
     Install-WindowsFeature -Name Web-Server, Web-Windows-Auth, BranchCache -IncludeManagementTools
 }
-
 Write-Host -ForegroundColor Green "[+] Install-PowerShell74X"
-iex (irm https://raw.githubusercontent.com/gwblok/2PintLabs/refs/heads/main/DeployR/TSScripts/Functions/Install-PowerShell74X.ps1)
+Function Install-PowerShell74X {
+    iex (irm https://raw.githubusercontent.com/gwblok/2PintLabs/refs/heads/main/DeployR/TSScripts/Functions/Install-PowerShell74X.ps1)
+}
 
 Write-Host -ForegroundColor Green "[+] Install-PowerShell76X"
-iex (irm https://raw.githubusercontent.com/2pintsoftware/2Pint-DeployR/refs/heads/main/Installs/Pre-Reqs/Install-PowerShell76X.ps1)
+function Install-PowerShell76X {
+    iex (irm https://raw.githubusercontent.com/2pintsoftware/2Pint-DeployR/refs/heads/main/Installs/Pre-Reqs/Install-PowerShell76X.ps1)
+}
 
 Write-Host -ForegroundColor Green "[+] Install-DotNetRuntimes80X"
 function Install-DotNetRuntimes80X {
